d0b8e0ec23e2cd363056a64c0f84893f96b2dc91
Josue Quispe <jquispepauc@unsa.edu.pe>
2025-10-02 22:44:03 -0500

    Implementando la clase Integrador, esta se encarga de realizar el método del trapecio el cual con un ciclo sumará las areas de los trapecios según los intervalos a ingresar

--- Lab03/Trapecio.java
+++ Lab03/Trapecio.java
@@ -10,4 +10,27 @@ public class Trapecio{
             return 2.0*x*x+3.0*x+0.5;
         }
     }
+
+    public static class Integrador{
+        private final Funcion f;
+        private final double a, b;
+        private final long n;
+
+        public Integrador(Funcion f, double a, double b, long n){
+            this.f = f;
+            this.a = a;
+            this.b = b;
+            this.n = n;
+        }
+
+        public double integrar() {
+            double h = (b - a) / (double) n;
+            double sum = 0.5 * (f.eval(a) + f.eval(b));
+            for (long i = 1; i < n; i++) {
+                double x = a + i * h;
+                sum += f.eval(x);
+            }
+            return sum * h;
+        }
+    }
 }
