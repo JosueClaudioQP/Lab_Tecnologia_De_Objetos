c7210f49ce014455f7554ce039a013717b485bb6
Yordano Boza <ybozap@unsa.edu.pe>
2025-10-04 20:09:29 -0500

    Se creo la funcion integrarPorTrapecioParalelo que divide el calculo de la integral en varias partes y las asigna a diferentes hilos. Cada hilo ejecuta integrarParcial para procesar un rango de puntos y al final se combinan los resultados parciales.

--- Lab04/trapecioPool.cpp
+++ Lab04/trapecioPool.cpp
@@ -16,3 +16,29 @@ void integrarParcial(double a, double h, long inicio, long fin, long double &res
     }
     resultado_parcial = suma;
 }
+
+long double integrarPoTrapecioParalelo(double a, double b, long n, int num_hilos) {
+    double h = (b - a) / n;
+    long double suma_total = 0.5L * (f(a) + f(b));
+
+    thread* hilos = new thread[num_hilos];
+    long double* resultados = new long double[num_hilos];
+
+    long pasos_por_hilo = n / num_hilos;
+
+    for (int i = 0; i < num_hilos; i++) {
+        long inicio = i * pasos_por_hilo + 1;
+        long fin = (i == num_hilos - 1) ? (n - 1) : ((i + 1) * pasos_por_hilo);
+        hilos[i] = thread(integrarParcial, a, h, inicio, fin, ref(resultados[i]));
+    }
+
+    for (int i = 0; i < num_hilos; i++) {
+        hilos[i].join();
+        suma_total += resultados[i];
+    }
+
+    delete[] hilos;
+    delete[] resultados;
+
+    return suma_total * h;
+}
