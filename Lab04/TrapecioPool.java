3fe0058cc58f47dc08b46fee231341bb7b2fffec
Josue Quispe <jquispepauc@unsa.edu.pe>
2025-10-04 22:18:59 -0500

    Creando la clase TrapecioPool con la estructura base y la función brindada

---
+++ Lab04/TrapecioPool.java
@@ -0,0 +1,10 @@
+public class TrapecioPool {
+
+    public static double f(double x) {
+        return 2.0 * x * x + 3.0 * x + 0.5;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
